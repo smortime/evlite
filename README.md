c8b986ff92a493dbeea69f1cb4ada4e57ae7f19b
Schuyler Mortimer <schuymortimer@gmail.com>
2022-02-12 17:08:56 -0800

    start of the REPL

---
+++ README.md
@@ -0,0 +1,5 @@
+# EVLite
+Toy implementation of SQLite written in rust. My dog's name is Evie so yeah...
+
+### Resources
+- [Architecture of SQLite](https://www.sqlite.org/arch.html)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to EVLite Beta\nType 'help' for usage hints");
+    loop {
+        match prompt("evlite> ").as_str() {
+            "quit" => {
+                println!("Goodbye"); 
+                break;
+            },
+            _ => println!("Invalid command"),
+        };
+        println!();
+    }
+}
+
+fn prompt(name: &str) -> String {
+    let mut user_input = String::new();
+    print!("{}", name);
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut user_input)
+        .expect("Error: could not read line");
+    user_input.trim().to_string()
 }
